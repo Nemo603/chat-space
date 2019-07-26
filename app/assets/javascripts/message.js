167c9791813ae26d96b34d10768bbe18a6acbc33
Nemo603 <nemotrace@gmail.com>
2019-07-26 17:20:37 +0900

    スクロール設定完了
    変わらないで

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,62 @@
+$(function(){
+  $(document).on('turbolinks:load', function(){
+    function bulidMessage(message){
+
+
+      var image
+      if (message.image != null) {  //imageがnull じゃ無い場合
+        image = `<img src="${message.image}" alt="画像">`
+      }else{
+        image = ''
+      }
+      // 三項演算子
+
+      var html = `  <div class="message">
+                      <div class="upper-message">
+                        <div class="upper-message__user-name">
+                          ${message.name}
+                        </div>
+                        <div class="upper-message__date">
+                          ${message.created_at}
+                        </div>
+                      </div>
+                      <div class="lower-message">
+                        <p class="lower-message__content">
+                        ${message.content}
+                        ${image}
+                        </p>
+                      
+                      </div>
+                    </div>`;
+      return html;
+      }
+
+      $('#new_message').on('submit',function(e){
+        e.preventDefault();
+        var formData = new FormData(this);
+        var url = $(this).attr('action')
+        $.ajax({
+          url: url,
+          type: "POST",
+          data: formData,
+          dataType: 'json',
+          processData: false,
+          contentType: false
+        })
+        .done(function(message){
+          var html = bulidMessage(message);
+          $('.center-text').append(html);
+          $('.form__message').val('');
+          var height = $('.text')[0].scrollHeight;
+          $('.text').animate({scrollTop:height});
+          
+          
+        
+      })
+      .fail(function(){
+        alert('送信が失敗しました');
+      })
+      return false;  //これで連続で送信ボタンを押せるようにしている
+    }); 
+  })
+});
