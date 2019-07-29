ddffabb8a89f678d99bd87e4e722605146378d9d
Nemo603 <nemotrace@gmail.com>
2019-07-29 19:42:30 +0900

    console.系消しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -6,7 +6,6 @@ $(function(){
         
     var image = message.image === null ? image = '' : image = `<img src="${message.image}" alt="画像">`
 
-    if (message.content && image) {
       var html = `  <div class="message" data-id="${message.id}">
                       <div class="upper-message">
                         <div class="upper-message__user-name">
@@ -24,41 +23,6 @@ $(function(){
                       
                       </div>
                     </div>`;
-      } else if  (message.content) {
-        var html = `  <div class="message" data-id="${message.id}">
-                      <div class="upper-message">
-                        <div class="upper-message__user-name">
-                          ${message.name}
-                        </div>
-                        <div class="upper-message__date">
-                          ${message.created_at}
-                        </div>
-                      </div>
-                      <div class="lower-message" >
-                        <p class="lower-message__content">
-                        ${message.content}
-                        </p>
-                      
-                      </div>
-                    </div>`;
-      } else if (image) {
-        var html = `  <div class="message" data-id="${message.id}">
-                      <div class="upper-message">
-                        <div class="upper-message__user-name">
-                          ${message.name}
-                        </div>
-                        <div class="upper-message__date">
-                          ${message.created_at}
-                        </div>
-                      </div>
-                      <div class="lower-message" >
-                        <p class="lower-message__content">
-                        ${image}
-                        </p>
-                      
-                      </div>
-                    </div>`;
-      };
       return html;
     }
   $('#new_message').on('submit',function(e){
@@ -74,7 +38,6 @@ $(function(){
       contentType: false
     })
   .done(function(message){
-    console.log(message);
     var html = bulidMessage(message);
     $('.message').append(html);
     $('#new_message')[0].reset();
@@ -94,11 +57,10 @@ $(function(){
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
       //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
       var last_message_id = $('.center-text').last().data('id')
-      console.log(last_message_id)
       var href = 'api/messages'
       $.ajax({
         //ルーティングで設定した通りのURLを指定
-        url: location.href,
+        url: window.location.href,
         //ルーティングで設定した通りhttpメソッドをgetに指定
         type: 'get',
         dataType: 'json',
@@ -106,9 +68,7 @@ $(function(){
         data: {id: last_message_id}  //formDataじゃないのでPresentDataなどがいらない
       })
       .done(function(messages) {
-          // console.log(messages.length);
           var message = messages.slice(-1)[0];
-          console.log(message)
           var html = bulidMessage(message);
           $('.center-text').append(html);
           $('#new_message')[0].reset();
@@ -116,7 +76,7 @@ $(function(){
           $('.text').animate({scrollTop:height});
       })
       .fail(function() {
-        console.log('自動更新できない');
+        alert("自動更新に失敗しました");
       });
       }
         clearInterval(reloadMessages);
