884bdc506ba19b7eb83e68a215486bfab1c9988b
Nemo603 <nemotrace@gmail.com>
2019-08-11 15:14:56 +0900

    修正完了2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,9 +4,10 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    @members = @group.users
     respond_to do |format|
       format.html
-      format.json
+      format.json { @messages = @messages.where("id > ?", params[:last_id]) }
     end
   end
 
